23c1ba3f1b23d4479e727c624501687de01ce483
cgx <a304775438@aliyun.com>
2018-04-22 16:15:54 +0800

    update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,12 +2,8 @@ git submodule update --init --recursive --depth 1
 
 cd current && npm run setup
 
-echo "building"
-grunt build && grunt prod
-
 cd ..
 echo "request password run script(sudo find ./ ! -path './versions/*' -type f -exec chmod 664 {} \;)"
 sudo find ./ ! -path "./versions/*" -type f -exec chmod 664 {} \;
-
 
 ghost setup
